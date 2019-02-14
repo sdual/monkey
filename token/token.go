d4ee2d0d9c505f68852c20fc10527393f8bcc50d
sdual <shr.nrht.0623@gmail.com>
2019-02-14 14:58:33 +0900

    first commit

---
+++ token/token.go
@@ -0,0 +1,28 @@
+package token
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
